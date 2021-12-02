d5bb3a95e6f152f360f030845397940ca92a186a
dh5104 <95384165+dh5104@users.noreply.github.com>
2021-12-02 00:16:51 -0500

    Day 2

--- AoC2021/D02/D02Y2021.cs
+++ AoC2021/D02/D02Y2021.cs
@@ -2,12 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
     using GlobalUtils;
     using GlobalUtils.Attributes;
 
-    [Exercise("")]
+    [Exercise("Day 2: Dive!")]
     class D02Y2021 : FileSelectionConsole, IExercise
     {
         public void Execute()
@@ -17,60 +15,56 @@
 
         protected override void Execute(string[] inputLines)
         {
-            int validCount = 0;
-            int validPart2Count = 0;
+            List<int> numbers = ParseUtils.InputAsInts(inputLines, false);
+            int horz = 0;
+            int depth = 0;
             foreach (string line in inputLines)
             {
-                PwTest rule = new PwTest(line);
-                if (rule.IsValid)
+                var a = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int distance = int.Parse(a[1]);
+                switch(a[0])
                 {
-                    validCount++;
-                }
-                
-                if (rule.IsValidPart2)
-                {
-                    validPart2Count++;
+                    case "forward":
+                        horz += distance;
+                        break;
+                    case "down":
+                        depth += distance;
+                        break;
+                    case "up":
+                        depth -= distance;
+                        break;
                 }
             }
 
-            Console.WriteLine($"{validCount} rules valid for part 1");
-            Console.WriteLine($"{validPart2Count} rules valid for part 2");
-        }
+            Console.WriteLine($"Part 1: Horizontal: {horz}, Depth: {depth}");
+            Console.WriteLine($"Answer: {horz * depth}");
 
-        public class PwTest
-        {
-            public int MinCount { get; set; }
-            public int MaxCount { get; set; }
-            public char RequiredChar { get; set; }
-            public string PasswordToValidate { get; set; }
-            public bool IsValid { get; set; }
-            public bool IsValidPart2 { get; set; }
-
-            public PwTest(string inputLine)
+            horz = 0;
+            depth = 0;
+            int aim = 0;
+            foreach (string line in inputLines)
             {
-                // 2-4 c: cdgdkcd
-                // [0]-[1] [2]: [3]
-                string[] parts = inputLine.Split(new char[] { ' ', ':', '-' }, StringSplitOptions.RemoveEmptyEntries);
-                this.MinCount = int.Parse(parts[0]);
-                this.MaxCount = int.Parse(parts[1]);
-                this.RequiredChar = char.Parse(parts[2]);
-                this.PasswordToValidate = parts[3];
-
-                int numInstRQ = this.PasswordToValidate.Where(c => c == this.RequiredChar).Count();
-                if (numInstRQ >= this.MinCount && numInstRQ <= this.MaxCount)
+                var a = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int distance = int.Parse(a[1]);
+                switch (a[0])
                 {
-                    this.IsValid = true;
-                }
+                    case "forward":
+                        horz += distance;
+                        depth += (distance * aim);
 
-                bool hasPrimary = this.PasswordToValidate[this.MinCount - 1] == RequiredChar;
-                bool hasSecondary = this.PasswordToValidate[this.MaxCount - 1] == RequiredChar;
-                
-                // yeah XOR baby!
-                if ((hasPrimary ^ hasSecondary))
-                {
-                    this.IsValidPart2 = true;
+                        break;
+                    case "down":
+                        aim += distance;
+                        break;
+                    case "up":
+                        aim -= distance;
+                        break;
                 }
             }
+
+            Console.WriteLine($"Part 2: Horizontal: {horz}, Depth: {depth}, Aim: {aim}");
+            Console.WriteLine($"Answer: {horz * depth}");
+            Console.WriteLine($" rules valid for part 2");
         }
     }
 }
