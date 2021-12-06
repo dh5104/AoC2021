0781ed45bed6a4d192578cc75bcca4fa069f9aa2
dh5104 <95384165+dh5104@users.noreply.github.com>
2021-12-06 11:38:18 -0500

    Day 5

--- AoC2021/D05/D05Y2021.cs
+++ AoC2021/D05/D05Y2021.cs
@@ -2,10 +2,12 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using GlobalUtils;
     using GlobalUtils.Attributes;
+    using GlobalUtils.Spatial;
 
-    [Exercise("")]
+    [Exercise("Day 5: Hydrothermal Venture")]
     class D05Y2021 : FileSelectionConsole, IExercise
     {
         public void Execute()
@@ -15,23 +17,86 @@
 
         protected override void Execute(string[] inputLines)
         {
-            List<int> numbers = ParseUtils.InputAsInts(inputLines, false);
-            List<long> longNumbers = ParseUtils.InputAsLongs(inputLines, false);
+            List<LineSegment> lines = new List<LineSegment>();
+            int minX = int.MaxValue;
+            int maxX = int.MinValue;
+            int minY = int.MaxValue;
+            int maxY = int.MinValue;
             foreach (string line in inputLines)
             {
+                char[] breakChars = { ' ', ',', '-', '>' };
                 if (!string.IsNullOrWhiteSpace(line))
                 {
-                    string[] parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string[] parts = line.Split(breakChars, StringSplitOptions.RemoveEmptyEntries);
+                    LineSegment s = new LineSegment(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]), int.Parse(parts[3]));
+                    lines.Add(s);
+                    if (s.X1 < minX)
+                    {
+                        minX = s.X1;
+                    }
+
+                    if (s.X1 > maxX)
+                    {
+                        maxX = s.X1;
+                    }
+
+                    if (s.X2 < minX)
+                    {
+                        minX = s.X2;
+                    }
+
+                    if (s.X2 > maxX)
+                    {
+                        maxX = s.X2;
+                    }
+
+                    if (s.Y1 < minY)
+                    {
+                        minY = s.Y1;
+                    }
+
+                    if (s.Y1 > maxY)
+                    {
+                        maxY = s.Y1;
+                    }
+
+                    if (s.Y2 < minY)
+                    {
+                        minY = s.Y2;
+                    }
+
+                    if (s.Y2 > maxY)
+                    {
+                        maxY = s.Y2;
+                    }
                 }
             }
 
-            Console.WriteLine("Part 1");
-            Console.WriteLine($"");
-            Console.WriteLine($"Ans: ");
+            long overlapCount = 0; 
+            for (int j = minY; j <= maxY; j++)
+            {
+                for (int i = minX; i <= maxX; i++)
+                {
+                    int thisPointCount = 0;
+                    foreach (var a in lines)
+                    {
+                        if (a.In(i, j))
+                        {
+                            thisPointCount++;
+                            if (2 <= thisPointCount)
+                            {
+                                overlapCount++;
+                                break;
+                            }
+                        }
+                    }
+                }
 
-            Console.WriteLine("Part 1");
-            Console.WriteLine($"");
-            Console.WriteLine($"Ans: ");
+                Console.WriteLine($"Y Line {j}: {overlapCount}");
+            }
+
+            Console.WriteLine("Part 2");
+            Console.WriteLine($"Ans: {overlapCount}");
         }
     }
 }
---
+++ GlobalUtils/Spatial/LineSegment.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GlobalUtils.Spatial
+{
+    public class LineSegment
+    {
+        public LineSegment(int x1, int y1, int x2, int y2)
+        {
+            if (x1 < x2)
+            {
+                this.X1 = x1;
+                this.Y1 = y1;
+                this.X2 = x2;
+                this.Y2 = y2;
+            }
+            else if (x1 > x2)
+            {
+                this.X1 = x2;
+                this.Y1 = y2;
+                this.X2 = x1;
+                this.Y2 = y1;
+            }
+            else if (x1 == x2 && y1 <= y2)
+            {
+                this.X1 = x1;
+                this.Y1 = y1;
+                this.X2 = x2;
+                this.Y2 = y2;
+            }
+            else
+            {
+                this.X1 = x2;
+                this.Y1 = y2;
+                this.X2 = x1;
+                this.Y2 = y1;
+            }
+
+            if (x1 == x2)
+            {
+                this.IsVertical = true;
+            }
+            else if (y1 == y2)
+            {
+                this.IsHorizontal = true;
+            }
+            else if (System.Math.Abs(x2 - x1) == System.Math.Abs(y2 - y1))
+            {
+                this.IsDiagonal = true;
+                this.DiagonalPoints = new List<GenericPoint>();
+                int numPoints = 1 + System.Math.Abs(x2 - x1);
+
+                bool increasing = this.Y1 < this.Y2;
+
+                // x is already assigned as going up based on the sorting above.
+                for (int i = 0; i < numPoints; i++)
+                {
+                    if (increasing)
+                    {
+                        this.DiagonalPoints.Add(new GenericPoint(this.X1 + i, this.Y1 + i));
+                    }
+                    else
+                    {
+                        this.DiagonalPoints.Add(new GenericPoint(this.X1 + i, this.Y1 - i));
+                    }
+                }
+            }
+        }
+
+        public int X1 { get; private set; }
+
+        public int Y1 { get; private set; }
+
+        public int X2 {  get; private set;}
+
+        public int Y2 {  get; private set;}
+
+        public bool IsVertical { get; private set; }
+
+        public bool IsHorizontal { get; private set; }
+
+        public bool IsDiagonal { get; private set; }
+
+        public List<GenericPoint> DiagonalPoints { get; private set; }
+
+        public bool In(int x, int y)
+        {
+            bool withinLine = false;
+            if (this.IsVertical && x == this.X1)
+            {
+                withinLine = (y >= this.Y1 && y <= this.Y2);
+            }
+            else if (this.IsHorizontal && y == this.Y1)
+            {
+                withinLine = (x >= this.X1 && x <= this.X2);
+            }
+            else if (this.IsDiagonal && x >= this.X1 && x <= this.X2)
+            {
+                withinLine = this.DiagonalPoints.Any(pt => pt.X == x && pt.Y == y);
+            }
+
+            return withinLine;
+        }
+    }
+}
